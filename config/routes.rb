b6708752ff7fdb3a89ec75a43ff5c90ca01f0942
Vincent Jim <vjim323@gmail.com>
2014-12-02 00:02:03 -0500

    created the review routes, controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :products
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
+  end
 
   resources :users, only: [:new, :create]
 
